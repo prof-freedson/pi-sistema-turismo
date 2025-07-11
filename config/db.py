64e5499615cf2e3aec5c372b5cd40fcc4f0d4991
RichardsonJPSilva <richardsonsilva1@gmail.com>
2025-07-11 20:34:33 -0300

    Configurações inicias do banco de dados

--- config/db.py
+++ config/db.py
@@ -1,1 +1,15 @@
 # Configuração do banco de dados
+import os
+from dotenv import load_dotenv
+import mysql.connector
+
+# Carrega variáveis do arquivo .env
+load_dotenv()
+
+def get_db_connection():
+    return mysql.connector.connect(
+        host=os.getenv('HOST'),
+        user=os.getenv('USER'),
+        password=os.getenv('PASSWORD'),
+        database=os.getenv('DATABASE')
+    )
