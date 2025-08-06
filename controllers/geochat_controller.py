98e158a3213845009f0df26ce70fe0d6c03f58ea
prof-freedson <freedson.prof@gmail.com>
2025-08-06 19:16:46 -0300

    Correção no chat

--- controllers/geochat_controller.py
+++ controllers/geochat_controller.py
@@ -343,8 +343,7 @@ class GeoChatController:
 
     def chat_page(self):
         """Renderiza a página do chat"""
-        if 'user_id' not in session:
-            return redirect(url_for('chat/index.html'))
+        # Permite acesso sem autenticação para demonstração
         return render_template('chat/index.html')
     
     def handle_chat(self):
